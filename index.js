984ff3c58e9e5f785d41c3347210682d8a67633f
uglyfeet <welch.antony@gmail.com>
2026-06-01 10:31:37 +0100

    board done

--- index.js
+++ index.js
@@ -16,16 +16,6 @@ let rows = 16;
 let columns = 16;
 
 
-let board;
-let boardWidth = tileSize * columns; // 32 * 16
-let boardHeight = tileSize * rows; // 32 * 16
-let context;
 
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.width = boardWidth;
-    board.height = boardHeight;
-    context = board.getContext("2d"); //used for drawing on the board
-}
 
